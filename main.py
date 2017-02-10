db9900f0b8d7ba2d58f3855368acb19551222761
Marcin Koprek <marcinkoprek@gmail.com>
2017-02-10 17:39:04 +0100

    changed code with position of buttons, kivy version comment added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+#[Kivy        ] v1.9.1
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 
-Window.size = (600,400)       #size of window
+Window.size = (600,400)
 
 
 class MainWindow(Widget):
