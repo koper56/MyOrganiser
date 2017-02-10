e3fc70475f712ae1e3ad63bd6630383f981e1a85
Marcin Koprek <marcinkoprek@gmail.com>
2017-02-10 13:54:48 +0100

    main window and simple buttons

---
+++ main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+
+class MainWindow(Widget):
+    pass
+
+
+class MainWindowApp(App):
+    def build(self):
+        return MainWindow()
+
+
+if __name__ == '__main__':
+    MainWindowApp().run()
