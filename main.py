3b20e920bd827f92a31a5b82d365fd90b3bff759
Marcin Koprek <marcinkoprek@gmail.com>
2017-02-20 14:10:29 +0100

    added move between windows, added back buttons

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-#[Kivy        ] v1.9.1
+#Kivy v1.9.1
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-Window.size = (600,400)
+Window.size = (400,650)
 
 
-class MainWindow(Widget):
+class MainWindow(Screen):
     pass
 
+class Button1(Screen):
+    pass
+
+class Button2(Screen):
+    pass
+
+class Button3(Screen):
+    pass
+
+class Button4(Screen):
+    pass
+
+#control move between screens
+screen_manager = ScreenManager()
+
+#define all screens of app
+screen_manager.add_widget(MainWindow(name='MainWindow'))
+screen_manager.add_widget(Button1(name='Button1'))
+screen_manager.add_widget(Button2(name='Button2'))
+screen_manager.add_widget(Button3(name='Button3'))
+screen_manager.add_widget(Button4(name='Button4'))
 
 class MainWindowApp(App):
     def build(self):
-        return MainWindow()
-
+        return screen_manager
 
 if __name__ == '__main__':
     MainWindowApp().run()
+
