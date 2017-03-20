23df82d75fb192011d64fb6a4bbfcc99ac85bdcc
Marcin Koprek <marcinkoprek@gmail.com>
2017-03-20 18:53:54 +0100

    added functions with data base: new data, print all data, print selected data, update base, delete data

--- sqlite.py
+++ sqlite.py
@@ -15,6 +15,13 @@ def create_table():
                       'exclusion TEXT, kind TEXT)')
 
 
+# Value of last ID number + 1
+def next_id():
+    last_id_num = db_cursor.execute('SELECT max(id) FROM clothes_data')
+    global next_id
+    next_id = last_id_num.fetchone()[0] + 1
+
+
 # Add new data to table
 def add_data_from_input():
     # Type data
@@ -37,32 +44,88 @@ def add_data_from_input():
         input_kind = input('Kind: ')
         return inputs
 
+    # Check typed data
     def check():
-        # Check data
-        print('Name:{}, Color_1:{}, Color_2:{}, Color_3:{}, Source:{}, '
-              'Description:{}. Exclusions:{}. Kind:{}\n\n'
-              'Correct?'.format(input_name, input_color_1, input_color_2, input_color_3, input_source, input_description, input_exclusion, input_kind))
+        print('Name: {}, Color_1: {}, Color_2: {}, Color_3: {}, Source: {}, '
+              'Description: {}. Exclusions: {}. Kind: {}\n\n'
+              'Correct?'.format(input_name, input_color_1, input_color_2,
+                                input_color_3, input_source, input_description,
+                                input_exclusion, input_kind))
 
-        check_input_data = input('(Y/N)')
+        check_input_data = input("'Y', 'N' or 'quit'\n")
 
         if check_input_data == 'Y':
 
-            # Send data to table
+            # Create new ID number
+            next_id()
+
+            # Send data to table with new ID number
             db_cursor.execute(
-                "INSERT INTO clothes_data VALUES('ID', '{}', '{}', '{}', "
-                "'{}', '{}', '{}', '{}', '{}')".format(input_name, input_color_1, input_color_2, input_color_3, input_source, input_description, input_exclusion, input_kind))
+                "INSERT INTO clothes_data VALUES({}, '{}', '{}', '{}', "
+                "'{}', '{}', '{}', '{}', '{}')"
+                    .format(next_id, input_name, input_color_1, input_color_2,
+                            input_color_3, input_source, input_description,
+                            input_exclusion, input_kind))
+
             # Add data to table
             connection.commit()
 
             # Close function
             db_cursor.close()
             connection.close()
+
         elif check_input_data == 'N':
             add_data_from_input()
+
+        elif check_input_data == 'quit':
+            pass
+
         else:
             print("'Y' or 'N'!")
             check()
+
     inputs()
     check()
 
 
+def print_all_data():
+    db_cursor.execute('SELECT * FROM clothes_data')
+    for row in db_cursor.fetchall():
+        print(row)
+
+
+def print_selected_data():
+    search_category = input('Select category of search in data base\nid, '
+                            'name, kind\n-->: ')
+    search_data = input('Data to search in data base: ')
+    db_cursor.execute('SELECT * FROM clothes_data WHERE {}={}'
+                      .format(search_category, search_data))
+
+    for row in db_cursor.fetchall():
+        print(row)
+
+
+def update_data_base():
+    category = input('Choose category to update: ')
+    old_value = input('Choose value from {} to update: '.format(category))
+    new_value = input('Set new value for {} in {}'.format(old_value, category))
+
+    db_cursor.execute('UPDATE clothes_data SET {} = {} WHERE {} = {}'
+                      .format(category, new_value, category, old_value))
+    # Add data to table
+    connection.commit()
+
+    # Close function
+    db_cursor.close()
+    connection.close()
+
+
+def delete_data():
+    value = input('Choose data ID number to delete: ')
+    db_cursor.execute('DELETE FROM clothes_data WHERE ID = {}'.format(value))
+    # Add data to table
+    connection.commit()
+
+    # Close function
+    db_cursor.close()
+    connection.close()
