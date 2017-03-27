2be1156d19ee274b41a4da17f2c4512cc0abf8dd
Marcin Koprek <marcinkoprek@gmail.com>
2017-03-27 10:17:10 +0000

    Update for data_base.py

--- color_palette.py
+++ color_palette.py
@@ -5,21 +5,9 @@ from tkinter.colorchooser import askcolor
 # choose color from color palette
 def get_color():
     color = askcolor()
-    print('RGB code:',color[0],'\nHEX code:',color[1])
-
-    # save color code in txt file
-    try:
-        with open('colordata.txt', mode='a', encoding='utf-8') as color_file:
-
-            # from color code choose hex color, saved in text file in new line
-            color_file.write(str(color[1]))
-            color_file.write('\n')
-            color_file.close()
-            if color_file.closed == True:
-                print('color code saved in text file')
-    except:
-        print('Error with text file')
-        pass
+    print('RGB code:', color[0], '\nHEX code:', color[1])
+    set_color = color[1]
+    return set_color
 
 
 Button(text='Select Color', command=get_color).pack()
