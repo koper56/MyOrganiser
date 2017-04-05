d3198a0bf641a082ae7bef03daa97211da4891db
Marcin Koprek <marcinkoprek@gmail.com>
2017-04-05 14:44:56 +0200

    Move buttons

--- camera_module.py
+++ camera_module.py
@@ -2,11 +2,10 @@
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 import time
 Builder.load_string('''
 <CameraClick>:
-    orientation: 'vertical'
     Camera:
         id: camera
         resolution: (640, 480)
@@ -15,17 +14,19 @@ Builder.load_string('''
         text: 'Turn on/off camera'
         on_press: camera.play = not camera.play
         size_hint_y: None
-        height: '48dp'
+        height: '50dp'
+        pos: 0, 100
     Button:
         text: 'Take photo'
         size_hint_y: None
-        height: '48dp'
-        on_press: root.take_photo()    
-        on_press: app.stop() 
+        height: '50dp'
+        pos: 0, 150
+        on_press: root.take_photo()
+        on_press: app.stop()
 ''')
 
 
-class CameraClick(BoxLayout):
+class CameraClick(FloatLayout):
     # Function to capture the images and give them the names
     # according to their captured date F. ex 'Captured as IMG_2017_04_05.png'.
     def take_photo(self):
