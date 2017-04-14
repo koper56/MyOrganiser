7c6a96d4d4226f27299b66bccdd470312111f063
Marcin Koprek <marcinkoprek@gmail.com>
2017-04-14 14:33:42 +0200

    Add work camera module with pop-up

--- camera_module.py
+++ camera_module.py
@@ -1,30 +1,45 @@
-# basic code from https://github.com/kivy/kivy/blob/master/examples/camera/main.py
-
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.popup import Popup
+from kivy.properties import StringProperty
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
-from kivy.base import stopTouchApp
 import time
-import os
 
 Builder.load_string('''
-<CameraClick>:
+<ConfirmPopup>:
+    cols:1
+	Label:
+		text: root.text
+	GridLayout:
+		cols: 1
+		size_hint_y: None
+		height: '44sp'
     Camera:
         id: camera
-        resolution: (480, 640)
+        resolution: (480, 600)
         play: True
-    Button:
-        text: 'Take photo'
-        color: 0.435, 0.725, 0.56, 1
-        background_color: 0, 0.26, 0.27, 1
-        size_hint_y: None
-        height: '50dp'
-        pos: 0, 500
-        on_press: root.take_photo()
+		Button:
+			text: 'TAKE PHOTO'
+			color: 0.435, 0.725, 0.56, 1
+            background_color: 0, 0.26, 0.27, 1
+            size_hint_y: None
+            height: '50dp'
+            pos: 500, 500
+			on_press: root.take_photo()
+			on_release: root.dispatch('on_answer')
 ''')
 
 
-class CameraClick(FloatLayout):
+class ConfirmPopup(GridLayout):
+    text = StringProperty()
+
+    def __init__(self, **kwargs):
+        self.register_event_type('on_answer')
+        super(ConfirmPopup, self).__init__(**kwargs)
+
+    def on_answer(self, *args):
+        pass
+
     # Function to capture the images and give them the names
     # according to their captured date F. ex 'Captured as IMG_2017_04_05.png'.
     def take_photo(self):
@@ -32,9 +47,18 @@ class CameraClick(FloatLayout):
         time_format = time.strftime("%Y_%m_%d")
         camera.export_to_png("Set_from_{}.png".format(time_format))
         print("Captured as 'Set_from_{}.png'".format(time_format))
-        # Code to stop camera_module after take a shot
 
 
-class CameraModule(App):
+class PopupTest(App):
     def build(self):
-        return CameraClick()
+        content = ConfirmPopup()
+        content.bind(on_answer=self._on_answer)
+        self.popup = Popup(title="Camera module",
+                           content=content,
+                           size_hint=(None, None),
+                           size=(480, 600),
+                           auto_dismiss=False)
+        self.popup.open()
+
+    def _on_answer(self, instance):
+        self.popup.dismiss()
