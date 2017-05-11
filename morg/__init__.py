7e04438db132c6c3e59ff27e8dd983b171d1d240
Marcin Koprek <marcinkoprek@gmail.com>
2017-05-11 12:24:28 +0200

    Add comments in __init__.py

--- morg/__init__.py
+++ morg/__init__.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+# Set directions for non-relative files
 SRC_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.normpath(os.path.join(SRC_DIR, ".."))
 ASSETS_DIR = os.path.join(ROOT_DIR, 'assets')
@@ -10,6 +11,7 @@ LOG_FILE_PATH = os.path.join(
 )
 
 
+# Initiating main.py function
 def run():
 
     from morg.main import MyOrganiser
